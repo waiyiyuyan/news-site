5461b65d0e95970d240d80b377577fda94cad130
waiyiyuyan <waiyiyuyan@gmail.com>
2026-06-05 11:50:21 +0800

    优化：单页面tab切换多新闻源，iOS样式

--- generate.js
+++ generate.js
@@ -2,41 +2,46 @@ const Parser = require("rss-parser");
 const fs = require("fs");
 
 const parser = new Parser();
+const config = JSON.parse(fs.readFileSync("./config.json", "utf8"));
+const tpl = fs.readFileSync("./template.html", "utf8");
 
-const RSS_URL = "https://feeds.bbci.co.uk/news/world/rss.xml";
+async function build() {
+    let navBtnHtml = "";
+    let allNewsHtml = "";
+    let keyArr = [];
 
-async function run() {
-    const feed = await parser.parseURL(RSS_URL);
+    // 循环抓取每一个rss
+    for(let item of config){
+        keyArr.push(`"${item.key}"`);
+        const feed = await parser.parseURL(item.rssUrl);
+        // 生成导航按钮，默认第一个active
+        const activeClass = keyArr.length ===1 ? "active" : "";
+        navBtnHtml += `<button data-key="${item.key}" class="${activeClass}">${item.name}</button>`;
 
-    let html = `
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>BBC News Feed</title>
-</head>
-<body>
-    <h1>BBC World News</h1>
-    <ul>
-`;
+        // 生成栏目新闻盒子，默认第一个显示
+        const showClass = keyArr.length ===1 ? "show news-wrap" : "news-wrap";
+        let listItem = "";
+        feed.items.slice(0,20).forEach(art=>{
+            listItem += `
+            <li>
+                <a href="${art.link}" target="_blank">${art.title}</a>
+                <small>${art.contentSnippet||""}</small>
+            </li>`
+        })
+        allNewsHtml += `
+        <div class="${showClass}" data-key="${item.key}">
+            <ul>${listItem}</ul>
+        </div>`
+    }
 
-    feed.items.slice(0, 20).forEach(item => {
-       html += `
-			<li>
-			  <a href="${item.link}" target="_blank">${item.title}</a><br>
-			  <small>${item.contentSnippet || ""}</small>
-			</li>
-			`;
-    });
+    // 替换占位符
+    let finalHtml = tpl
+        .replace("{{NAV_BTN}}", navBtnHtml)
+        .replace("{{ALL_NEWS}}", allNewsHtml)
+        .replace("{{KEY_LIST}}", `[${keyArr.join(",")}]`);
 
-    html += `
-    </ul>
-</body>
-</html>
-`;
-
-    fs.writeFileSync("index.html", html);
-    console.log("生成完成：index.html");
+    fs.writeFileSync("index.html", finalHtml);
+    console.log("生成完成 index.html");
 }
 
-run();
+build();
